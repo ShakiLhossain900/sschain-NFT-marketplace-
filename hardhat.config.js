e35e2a1f5d9f9dd0001d2afd16774f6d26bb4713
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-07-11 21:11:11 +0600

    adding

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc-amoy.polygon.technology/";
+const NEXT_PUBLIC_PRIVATE_KEY = "0894eb8d94db991870c9608f0a1d290efb5702c87e72b81047410e47434ebb6a";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    polygon_amoy: {
+      url: NEXT_PUBLIC_POLYGON_MUMBAI_RPC,
+      accounts: [`0x${NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
+  },
+};
